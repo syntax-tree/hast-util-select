b611126c87daf5d4182ff0fbc4ee48cc13b14e04
Titus Wormer <tituswormer@gmail.com>
2019-05-25 12:20:41 +0200

    Remove superfluous `xo` rules

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 'use strict'
 
+/* eslint-disable import/no-unassigned-import */
 require('./matches')
 require('./select')
 require('./select-all')
 require('./all')
 require('./svg')
+/* eslint-enable import/no-unassigned-import */
